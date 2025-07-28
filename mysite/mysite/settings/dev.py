91edb7dac7c48b29e82598fcd038bcc6e4a191ac
cactegra <cactegra@protonmail.com>
2025-07-28 15:58:20 +0200

    django/wagtail install

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-@ep+xn2(b0+k2e$9kb((w_#f_!=^gzeh4whzuh(vzr@94o#j!a"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
