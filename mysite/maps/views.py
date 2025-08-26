9421633b0849d70ffe9e34230e7665b964b32b9e
cactegra <cactegra@protonmail.com>
2025-08-26 12:00:14 -0400

    leaflet app and view, TODO missing template

--- mysite/maps/views.py
+++ mysite/maps/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.views.generic import TemplateView
+
+class MainPageView(TemplateView):
+    template_name = 'maps/home.html'
+    def get_context_data(self, **kwargs):
+        from maps.models import Location
+
+        context = super(MainPageView, self).get_context_data(**kwargs)
+        context['locations'] = Location.objects.all()
+        return context
--- mysite/mysite/settings/base.py
+++ mysite/mysite/settings/base.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django.contrib.gis",
     "wagtailgeowidget",
+    "leaflet",
     "maps",
 ]
 
